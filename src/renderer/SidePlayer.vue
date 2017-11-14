aec60410f62c69fc17ca600e353a3ce75cf1acc6
Luc Libralesso <libralesso.l@gmail.com>
2017-11-14 14:35:28 +0100

    add vue-youtube-embed component and made sidePlayer component use it

---
+++ src/renderer/SidePlayer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="side_player">
+    <h1>Side Player Component</h1>
+    <youtube :video-id="videoId" @ready="ready" @playing="playing"></youtube>
+  </div>
+</template>
+<script>
+  import Vue from 'vue'
+  import VueYouTubeEmbed from 'vue-youtube-embed'
+  Vue.use(VueYouTubeEmbed)
+
+  export default {
+    name: 'sidePlayer',
+    data() {
+      return {
+        videoId: 'yKizp0pDFtA'
+      }
+    },
+    methods: {
+      ready(player) {
+        this.player = player
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,14 +3,25 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
 import App from './App'
+import SidePlayer from './SidePlayer'
 
 Vue.use(VueMaterial)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  components: { App },
-  template: '<App/>'
-}).$mount('#app')
+// IF MAIN PAGE
+if ( document.querySelector('#app') ) {
+  /* eslint-disable no-new */
+  new Vue({
+    components: { App },
+    template: '<App/>'
+  }).$mount('#app')
+}
+
+if ( document.querySelector('#side_player') ) {
+  new Vue({
+    components: {SidePlayer},
+    template: '<SidePlayer/>'
+  }).$mount('#side_player')
+}
